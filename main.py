d247a020d7c16c1900d0ae6be146dcad711bb43e
ksteeeeezy <twan001@ucr.edu>
2017-11-11 09:22:55 -0800

    Testing 1,2,3

---
+++ main.py
@@ -0,0 +1,41 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "This is math " ,20+10 # comma is for no endline
+print "Testing"
+cars = 3.0 # variables do not have to decalred a specific type
+print cars # printing variables do not need ""
+string = "Hello"
+print string
+print cars/2
+boolean = False
+while not(boolean):
+	print "My first while loop that I have created"
+	boolean = True;
+if (3 == 3):
+	print "Hello"
+
+#for i = 0 
+
+print """
+What is going on here? What does triple quotation marks mean? 
+I think it might just be for spacing.
+"""
+
+print "How old are you? ", # no endline
+age = raw_input()
+
+print "Hall tall are you?" #has endline
+height = raw_input()
+
+counter = 1;
+while counter != 5:
+	print "Fuck Kenneth and Tommy"
+	counter = counter + 1
+
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for arrays in array:
+	print(arrays)
